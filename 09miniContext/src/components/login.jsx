6f32516ea7ea90d29b58ffc974b55ee8886dc082
DurlabhDewangan <durlabhdewangan59@gmail.com>
2025-05-09 15:22:25 +0530

    context discussion

---
+++ 09miniContext/src/components/login.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Login() {
+    const [username, setUsername] = useState('');
+    const [pass, setPass] = useState('');
+
+    const { setUser } = useContext(UserContext);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUser({ username, password: pass });
+    };
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <input 
+                type="text" 
+                value={username} 
+                onChange={(e) => setUsername(e.target.value)} 
+                placeholder="Username" 
+            />
+            <input 
+                type="password" 
+                value={pass} 
+                onChange={(e) => setPass(e.target.value)} 
+                placeholder="Password" 
+            />
+            <button onClick={handleSubmit}>Submit</button>
+        </div>
+    );
+}
+
+export default Login;
+
