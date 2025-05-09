c32f7b68eff35e0c627759caa1ab0a57225534c9
DurlabhDewangan <durlabhdewangan59@gmail.com>
2025-05-09 12:18:57 +0530

    React router crash course completed

---
+++ 08reactRouter/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/home/home'
+import Header from './components/header/header'
+import Footer from './components/footer/footer'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
